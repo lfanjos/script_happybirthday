65010d7cd3951871cf9c5b1af98786f5d5eba846
Lucas Felipe dos Anjos <58798958+lfanjos@users.noreply.github.com>
2022-03-21 18:56:35 -0300

    First commit

---
+++ hp_script.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import smtplib
+import datetime
+from email.message import EmailMessage
+
+csv = pd.read_csv("happy_birthday.csv")
+today = datetime.datetime.now().strftime("%d/%m")
+yearnow = datetime.datetime.now().strftime("%Y")
+
+
+def send_email(to, message, subject):
+    # Preparando o Email
+    email = EmailMessage()
+    email['from'] = 'Lucas Felipe'
+    email['to'] = to
+    email['subject'] = subject
+    email.set_content(message)
+
+    # Enviando Email
+    with smtplib.SMTP("smtp.mailtrap.io", 2525) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login("9cf3dd4a1cc396", "c16c8dd61940d8")
+        smtp.send_message(email)
+        print(f"Email enviado para {to} com sucesso.")
+
+
+for index, item in csv.iterrows():
+    bday = datetime.datetime.strptime(item['data'], '%d/%m/%Y').strftime("%d/%m")
+
+    if bday == today and yearnow != item['last_gz']:
+        send_email(item['email'], item['msg'], f'Feliz Aniversário {item["nome"]}')
+    else:
+        print("Sem aniversários hoje")
+
+
+
+
+
